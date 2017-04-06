9f03b40dc104230859dc5042f384c31240c67c30
mamonreal <ma.monreal@alumnos.urjc.es>
2017-04-06 09:59:00 +0200

    metodo main

--- Analizador/src/analizador/Analizador.java
+++ Analizador/src/analizador/Analizador.java
@@ -12,10 +12,28 @@ package analizador;
 public class Analizador {
 
     /**
-     * @param args the command line arguments
+     * @param argv the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] argv) {
+        if (argv.length == 0) {
+            System.out.println("Inserta nombre de archivo\n" + "( Usage : java Analizador <inputfile> )");
+        } else {
+            for (int i = 0; i < argv.length; i++) {
+                AnalizadorLexico lexico = null;
+                try {
+                    lexico = new AnalizadorLexico(new java.io.FileReader(argv[i]));
+                    parser sintactico = new parser(lexico);
+                    sintactico.parse();
+                } catch (java.io.FileNotFoundException e) {
+                    System.out.println("Archivo \"" + argv[i] + "\" no encontrado.");
+                } catch (java.io.IOException e) {
+                    System.out.println("Error durante la lectura del" + " archivo \"" + argv[i] + "\".");
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    System.out.println("Excepcion:");
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-    
 }
