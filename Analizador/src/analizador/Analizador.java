79ca911f358750ab0839a73fa61d348b0dfe4cc7
ElenaMateos193 <elenam19396@gmail.com>
2017-04-08 22:40:26 +0200

    Memoria y main cambiado

--- Analizador/src/analizador/Analizador.java
+++ Analizador/src/analizador/Analizador.java
@@ -15,25 +15,25 @@ public class Analizador {
      * @param argv the command line arguments
      */
     public static void main(String[] argv) {
-//        if (argv.length == 0) {
-//            System.out.println("Inserta nombre de archivo\n" + "( Usage : java Analizador <inputfile> )");
-//        } else {
-//            for (int i = 0; i < argv.length; i++) {
+        if (argv.length == 0) {
+            System.out.println("Inserta nombre de archivo\n" + "( Usage : java Analizador <inputfile> )");
+        } else {
+            for (int i = 0; i < argv.length; i++) {
                 AnalizadorLexico lexico = null;
                 try {
-                    lexico = new AnalizadorLexico(new java.io.FileReader("/media/miguel/0914-ABE8/Universidad/3º/Procesadores del Lenguaje/Practica grupal 2/Procesadores-del-lenguaje/Analizador/src/analizador/input4 ERRONEO.txt"));
+                    lexico = new AnalizadorLexico(new java.io.FileReader(argv[i]));
                     parser sintactico = new parser(lexico);
                     Object resultado = sintactico.parse().value;
-//                } catch (java.io.FileNotFoundException e) {
-//                    System.out.println("Archivo \"" + argv[i] + "\" no encontrado.");
-//                } catch (java.io.IOException e) {
-//                    System.out.println("Error durante la lectura del" + " archivo \"" + argv[i] + "\".");
-//                    e.printStackTrace();
+                } catch (java.io.FileNotFoundException e) {
+                    System.out.println("Archivo \"" + argv[i] + "\" no encontrado.");
+                } catch (java.io.IOException e) {
+                    System.out.println("Error durante la lectura del" + " archivo \"" + argv[i] + "\".");
+                    e.printStackTrace();
                 } catch (Exception e) {
                     System.out.println("Excepcion:");
                     e.printStackTrace();
                 }
-//            }
-//        }
+            }
+        }
     }
 }
