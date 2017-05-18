dcd8c776737e42d595210ad57c300b6812f885f7
Helena García González <helogargon@gmail.com>
2017-05-18 12:40:09 +0200

    A todo gass 4

---
+++ Analizador.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analizadorsobresaliente;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ */
+public class Analizador {
+
+    public static void main(String argv[]) {
+        if (argv.length == 0) {
+            System.out.println("Inserta nombre de archivo\n"
+                    + "( Usage : java Analizador <inputfile> )");
+        } else {
+            for (int i = 0; i < argv.length; i++) {
+                AnalizadorLexico lexico = null;
+                try {
+                    lexico = new AnalizadorLexico(new java.io.FileReader(argv[i]));
+                    parser sintactico = new parser(lexico);
+		    sintactico.inicio(argv[i]);
+                    sintactico.parse();
+                } catch (java.io.FileNotFoundException e) {
+                    System.out.println("Archivo \"" + argv[i] + "\" no encontrado.");
+                } catch (java.io.IOException e) {
+                    System.out.println("Error durante la lectura del"
+                            + " archivo \"" + argv[i] + "\".");
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    System.out.println("Excepcion:");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
