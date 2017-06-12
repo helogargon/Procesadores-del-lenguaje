8f37c9fe2f422ff0187e81991a0a3558d84de429
ElenaMateos193 <elenam19396@gmail.com>
2017-06-13 01:20:24 +0200

    :squirrel: Marcado donde falla, linea y columna

---
+++ EjemploAprobado.c
@@ -0,0 +1,27 @@
+void intercambio (int v1, int v2){
+	int auxiliar;
+
+	auxiliar = v1;
+	v1 = v2;
+	v2 = auxiliar;
+}
+
+float formula (float p1, int p2){
+	float auxiliar;
+
+	intercambio( p1 * 10, p2);
+	ESTA LINEA DA PROBLEMAS auxiliar = auxiliar + -3 + p2 % 045 EL PROBLEMA EMPIEZA AQUÍ* 0-3 / 0+67;
+	auxiliar = auxiliar  - (+98 - p1 * 0x+ABD % 0xFC4D);
+	ESTA LINEA DA PROBLEMAS auxiliar = -3.98 + p2 % 045.27 EL PROBLEMA EMPIEZA AQUÍ * 0-3.11 / 0+67.34;
+	auxiliar = auxiliar - (+98.5 - p1 * 0x+ABD.EE % 0xFC4D.F4);
+	return auxiliar * 0x-FF;
+}
+
+void Main (int args){
+	int entero1, entero2;
+	float real1, real2;
+
+	printf('Esto es el programa \'principal\'...');
+	intercambio(entero2, entero1);
+	real1 = formula(real2,entero1);
+}
