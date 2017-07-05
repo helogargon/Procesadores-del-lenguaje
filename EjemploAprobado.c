a189b6728afceac755324ea3c211e09be716f027
Helena García González <helogargon@gmail.com>
2017-07-05 12:16:18 +0200

    ejemplo corregido

--- EjemploAprobado.c
+++ EjemploAprobado.c
@@ -10,9 +10,9 @@ float formula (float p1, int p2){
 	float auxiliar;
 
 	intercambio( p1 * 10, p2);
-	ESTA LINEA DA PROBLEMAS auxiliar = auxiliar + -3 + p2 % 045 EL PROBLEMA EMPIEZA AQUÍ* 0-3 / 0+67;
+	auxiliar = auxiliar + -3 + p2 % 045 * 0-3 / 0+67;
 	auxiliar = auxiliar  - (+98 - p1 * 0x+ABD % 0xFC4D);
-	ESTA LINEA DA PROBLEMAS auxiliar = -3.98 + p2 % 045.27 EL PROBLEMA EMPIEZA AQUÍ * 0-3.11 / 0+67.34;
+	auxiliar = -3.98 + p2 % 045.27 * 0-3.11 / 0+67.34;
 	auxiliar = auxiliar - (+98.5 - p1 * 0x+ABD.EE % 0xFC4D.F4);
 	return auxiliar * 0x-FF;
 }
