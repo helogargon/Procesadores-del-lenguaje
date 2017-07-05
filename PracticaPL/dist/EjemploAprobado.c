2155b0e79e5467ad9ae1dc2a17dd81784b7e2a00
Helena García González <helogargon@gmail.com>
2017-07-05 12:14:17 +0200

    stilos añadidos a PracticaPL y traduccion.cup

--- PracticaPL/dist/EjemploAprobado.c
+++ PracticaPL/dist/EjemploAprobado.c
@@ -9,15 +9,12 @@ void intercambio (int v1, int v2){
 float formula (float p1, int p2){
 	float auxiliar;
 
-/*
 	intercambio( p1 * 10, p2);
 	auxiliar = auxiliar + -3 + p2 % 045 * 0-3 / 0+67;
 	auxiliar = auxiliar  - (+98 - p1 * 0x+ABD % 0xFC4D);
 	auxiliar = -3.98 + p2 % 045.27 * 0-3.11 / 0+67.34;
 	auxiliar = auxiliar - (+98.5 - p1 * 0x+ABD.EE % 0xFC4D.F4);
 	return auxiliar * 0x-FF;
-*/
-
 }
 
 void Main (int args){
